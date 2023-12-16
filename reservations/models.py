663748c5e5b6854c7a7977c8b4b9044b2462fd1f
aleksandrasucho <aleksandrasucho@gmail.com>
2023-12-16 13:43:34 +0000

    Add table capacity to reservations models.py

--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+# Provides a list of choices for table capacity.
+CAPACITY = ((2, "2"), (4, "4"), (6, "6"), (8, "8"))
 
 # Create your models here.
 
+class Table(models.Model):
+    # Table details
+    table_number = models.IntegerField(unique=True)
+    number_of_seats = models.IntegerField()
+    capacity = models.IntegerField(choices=CAPACITY, default=2)
+    
+    # String representation of a Table instance
+    def __str__(self):
+        return f"Table {self.table_number} - Seats: {self.number_of_seats}"
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    # Basic reservation details
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    notes = models.TextField(null=True, blank=True)
+    table = models.ForeignKey(Table, related_name="reservations", on_delete=models.CASCADE, blank=True,)
+    number_of_guests = models.IntegerField()
+    
+    # String representation of a Reservation instance
+    def __str__(self):
+        return f"{self.name} - {self.date} {self.time}"
