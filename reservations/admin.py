a411f744089361c9f2be81d6d8ef0d53d6f6fceb
aleksandrasucho <aleksandrasucho@gmail.com>
2023-12-16 18:32:22 +0000

    Update views and templates

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Table, Reservation
+from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
 # Register your models here.
+
+admin.site.register(Table)
+admin.site.register(Reservation)
--- restaurant/settings.py
+++ restaurant/settings.py
@@ -53,8 +53,6 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
-    
-    
     'reservations',
     
 ]
