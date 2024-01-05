03f8fa9afd154c4e101c3c1e55c6f17e44e02cd7
aleksandrasucho <aleksandrasucho@gmail.com>
2024-01-05 15:14:39 +0000

    Update middleware

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -68,7 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    #'allauth.account.middleware.AccountMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'restaurant.urls'
