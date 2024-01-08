5ab0336439352a2fb2860a42cd4c53c40f96c81d
aleksandrasucho <aleksandrasucho@gmail.com>
2024-01-08 19:21:25 +0000

    Add Handle GET requests to views.py

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import HomeView, MenuView, AddReservation, InformationView
+from .views import HomeView, MenuView, AddReservation, InformationView, UpdateReservation, DeleteReservation
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('add_reservation/', AddReservation.as_view(), name="add_reservation"),
     path('information/', InformationView.as_view(), name="information"),
     path('accounts/', include('allauth.urls')),
+    path('update_reservation/<int:pk>/', UpdateReservation.as_view(), name="update_reservation"),
+    path('delete_reservation/<int:pk>/', DeleteReservation.as_view(), name="delete_reservation"),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.http import Http404
+from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.utils.decorators import method_decorator
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ReservationForm
@@ -21,60 +23,75 @@ class MenuView(generic.TemplateView):
 
 @method_decorator(login_required(login_url='account_login'), name='dispatch')
 class AddReservation(generic.CreateView):
+    """View to handle the addition of reservations."""
     template_name = 'add_reservation.html'
     model = Reservation
     form_class = ReservationForm
-    success_url = reverse_lazy('home')  # Use the correct name for the 'home' view
+    success_url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        """Handle GET requests."""
+        if request.user.is_authenticated:
+            if request.user.profile.role.id == 1:
+                return render(
+                    request,
+                    'profiles/access_denied.html',
+                )
+            else:
+                form = self.form_class()
+                return render(
+                    request,
+                    self.template_name,
+                    {'form': form},
+                )
+        else:
+            return render(
+                request,
+                'account/login.html',
+            )
 
     def form_valid(self, form):
-        reservation = form.save(commit=False)
-
-        # Retrieve date and time from cleaned_data
-        date = form.cleaned_data.get('date')
-        time = form.cleaned_data.get('time')
-
-        # Check if the selected table is available for the specified date and time
-        if reservation.table and Reservation.objects.filter(table=reservation.table, date=date, time=time).exists():
-            # Table is already booked at the specified date and time
-            raise ValidationError('Sorry, the selected table is not available for the specified date and time.')
-
-        # Get the selected table from the form and assign it to the reservation
-        selected_table = form.cleaned_data.get('table', None)
-        if selected_table:
-            reservation.table = selected_table
-        else:
-            # Assign table with the lowest capacity
-            guests = form.cleaned_data['number_of_guests']
-
-            # Filter tables with capacity greater or equal to the number of guests
-            tables_with_capacity = list(Table.objects.filter(
-                capacity__gte=guests
-            ))
-
-            # Get bookings on specified date and time
-            bookings_on_requested_date = Reservation.objects.filter(
-                date=date, time=time)
-
-            # Iterate over bookings to get tables not booked
-            for booking in bookings_on_requested_date:
-                for table in tables_with_capacity:
-                    if table.table_number == booking.table.table_number:
-                        tables_with_capacity.remove(table)
-                        break
-            
-            if tables_with_capacity:
-                lowest_capacity_table = min(tables_with_capacity, key=lambda table: table.capacity)
-                reservation.table = lowest_capacity_table
-            else:
-                pass
-
-        # Save the reservation to the database
-        reservation.save()
-
-        # Pass the reservation object to the success_url template
-        self.object = reservation
-
+        """Handle form validation for adding a reservation."""
+        form.instance.user = self.request.user
         return super().form_valid(form)
+
+class UpdateReservation(generic.edit.UpdateView):
+    template_name = 'update_reservation.html'
+    model = Reservation
+    form_class = ReservationForm
+    success_url = reverse_lazy('reservation:view')
+
+    def get_object(self, queryset=None):
+        reservation = super().get_object(queryset=queryset)
+        if self.request.user.is_staff or self.request.user == reservation.user:
+            return reservation
+        else:
+            raise Http404("You are not authorized to edit this reservation.")
+
+    def form_valid(self, form):
+        date = form.cleaned_data['date']
+        time = form.cleaned_data['time']
+        guests = form.cleaned_data['number_of_guests']
+
+        messages.success(self.request, f'Booking updated for {guests} guests on {date} at {form.instance.get_time_display()}')
+        return super(UpdateReservation, self).form_valid(form)
+
+class DeleteReservation(generic.edit.DeleteView):
+    template_name = 'delete_reservation.html'
+    model = Reservation
+    success_url = reverse_lazy('reservation:view')
+
+    def get_object(self, queryset=None):
+        reservation = super().get_object(queryset=queryset)
+        if self.request.user.is_staff or self.request.user == reservation.user:
+            return reservation
+        else:
+            raise Http404("You are not authorized to edit this reservation.")
+
+    def delete(self, request, *args, **kwargs):
+        reservation = self.get_object()
+        messages.success(self.request, f'Booking cancelled for {reservation.number_of_guests} guests on {reservation.date} at {reservation.get_time_display()}')
+        return super().delete(request, *args, **kwargs)
 
 class InformationView(generic.TemplateView):
     template_name = 'information.html'
